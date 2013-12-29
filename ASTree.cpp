d951cb7b69af0418d78dfa6edaa70aef3f7abeae
charlie tang <charlietang98@gmail.com>
2013-12-29 05:59:51 -0500

    it skipped LOAD_FAST, LOAD_GLOBAL

--- ASTree.cpp
+++ ASTree.cpp
@@ -1469,8 +1469,6 @@ PycRef<ASTNode> BuildFromCode(PycRef<PycCode> code, PycModule* mod)
                     blocks.pop();
                     curblock = blocks.top();
                     curblock->append(prev.cast<ASTNode>());
-
-                    bc_next(source, mod, opcode, operand, pos);
                 }
             }
             break;
