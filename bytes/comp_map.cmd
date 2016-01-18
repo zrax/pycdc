63bd3376b78e0f1d528c2829db6d4302646bd649
Djamana <cw2k@gmx.de>
2016-01-19 00:30:40 +0100

    Added * generated Phython map files * Visual Studio 2015 Projectfiles * Win32-Release Binaries

---
+++ bytes/comp_map.cmd
@@ -0,0 +1,6 @@
+@echo creating python_10.cpp  to  python_35.cpp  
+@echo so you don't get error
+@echo LNK2019	symbol ""int __cdecl python_35_map(int)" (?python_35_map@@YAHH@Z)" in Funktion ""int __cdecl Pyc::ByteToOpcode(int,int,int)" (?ByteToOpcode@Pyc@@YAHHHH@Z)"\bytecode.obj
+@echo anymore when compiling.
+@echo.
+comp_map.py . ..\
