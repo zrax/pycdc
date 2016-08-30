2178b103fc2218664f9afea4ed5accbb122946f4
Michael Hansen <zrax0111@gmail.com>
2016-08-30 11:23:10 -0700

    Change message if all tests "passed"

--- pycdc_test.sh
+++ pycdc_test.sh
@@ -20,9 +20,15 @@ do
         echo -ne "\033[32m.\033[m"
     fi
 done
-echo -e "\n\n$fails tests failed:"
-for ((i=0; i<${#files[@]}; i++))
-do
-    echo -e "\t\033[31m${files[i]}\033[m"
-    echo -e "${errors[i]}\n"
-done
+
+if (( $fails == 0 ))
+then
+    echo -e "\n\nAll tests passed!"
+else
+    echo -e "\n\n$fails tests failed:"
+    for ((i=0; i<${#files[@]}; i++))
+    do
+        echo -e "\t\033[31m${files[i]}\033[m"
+        echo -e "${errors[i]}\n"
+    done
+fi
