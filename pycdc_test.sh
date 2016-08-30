830bb0d31e3a778797399f5469b2bedff3915030
Michael Hansen <zrax0111@gmail.com>
2016-08-30 11:19:10 -0700

    Some fixes/cleanup for the test script

--- pycdc_test.sh
+++ pycdc_test.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
+testdir="$1"
 mkdir -p tests
 
 fails=0
 files=()
 errors=()
-for f in $1/*.pyc
+for f in "$testdir"/*.pyc
 do
-    base=tests/$( basename "$f" )
-    stderr=$( ./pycdc "$f" 2>$base.err 1>$base.src )
-    if [ "$?" -eq "0" -a -z "$stderr" ]
+    base="tests/$(basename "$f")"
+    ./pycdc "$f" 2>"$base.err" 1>"$base.src"
+    if (( $? )) || [[ -s "$base.err" ]]
     then
-        echo -ne "\033[32m.\033[m"
-    else
         let fails+=1
-        files=("${files[@]}" "$f")
-        errors=("${errors[@]}" "$stderr")
+        files+=("$f")
+        errors+=("$(cat "$base.err")")
         echo -ne "\033[31m.\033[m"
+    else
+        echo -ne "\033[32m.\033[m"
     fi
 done
 echo -e "\n\n$fails tests failed:"
 for ((i=0; i<${#files[@]}; i++))
 do
     echo -e "\t\033[31m${files[i]}\033[m"
-echo -e "${errors[i]}\n"
+    echo -e "${errors[i]}\n"
 done
