7efcf04bcdc96101ba8799f9269819b37a450d6b
Michael Hansen <zrax0111@gmail.com>
2016-09-03 11:05:03 -0700

    Display KW only arg count in pycdas output

--- pycdas.cpp
+++ pycdas.cpp
@@ -74,6 +74,8 @@ void output_object(PycRef<PycObject> obj, PycModule* mod, int indent)
             iprintf(indent + 1, "File Name: %s\n", codeObj->fileName()->value());
             iprintf(indent + 1, "Object Name: %s\n", codeObj->name()->value());
             iprintf(indent + 1, "Arg Count: %d\n", codeObj->argCount());
+            if (mod->majorVer() >= 3)
+                iprintf(indent + 1, "KW Only Arg Count: %d\n", codeObj->kwOnlyArgCount());
             iprintf(indent + 1, "Locals: %d\n", codeObj->numLocals());
             iprintf(indent + 1, "Stack Size: %d\n", codeObj->stackSize());
             iprintf(indent + 1, "Flags: 0x%08X", codeObj->flags());
