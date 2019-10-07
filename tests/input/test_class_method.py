ab58fba23b792a9d2c97006830df8c5e5e6cb653
Michael Hansen <zrax0111@gmail.com>
2019-10-07 12:00:30 -0700

    Move more tests into the new framework, including current xfails.

--- tests/input/test_class_method.py
+++ tests/input/test_class_method.py
@@ -2,7 +2,7 @@ class MyClass:
     def method(self, i):
         if i is 5:
             print 'five'
-        elif not (i is 2):
+        elif i is not 2:
             print 'not two'
         else:
             print '2'
--- tests/input/test_divide_future.py
+++ tests/input/test_divide_future.py
@@ -1,6 +1,11 @@
 from __future__ import division
 
-print '  1 // 2   =', 1 // 2
-print '1.0 // 2.0 =', 1.0 // 2.0
-print '  1 /  2   =', 1 / 2
-print '1.0 /  2.0 =', 1.0 / 2.0
+ai = 1
+bi = 2
+af = 1.5
+bf = 2.5
+
+print '  1 // 2   =', ai // bi
+print '1.5 // 2.5 =', af // bf
+print '  1 /  2   =', ai / bi
+print '1.5 /  2.5 =', af / bf
