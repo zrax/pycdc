44af6a2c04295f7b62d531258bd7176462970f07
Michael Hansen <zrax0111@gmail.com>
2019-10-07 20:57:43 -0700

    Finish removing old tests and old "test" framework.

---
+++ tests/input/test_functions_py3.py
@@ -0,0 +1,26 @@
+def x0():
+    pass
+
+def x1(arg1):
+    pass
+
+def x2(arg1, arg2):
+    pass
+
+def x3a(*args):
+    pass
+
+def x3b(**kwargs):
+    pass
+
+def x3c(*args, **kwargs):
+    pass
+
+def x4a(foo, bar = 1, bla = 2, *args):
+    pass
+
+def x4b(foo, bar = 1, bla = 2, **kwargs):
+    pass
+
+def x4c(foo, bar = 1, bla = 2, *args, **kwargs):
+    pass
