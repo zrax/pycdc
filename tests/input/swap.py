a77d76e82e37841418d5440ad1dce41bfc18eb4b
ddouworld <zha2376714571@gmail.com>
2024-06-01 12:28:50 +0800

    Add support for swap bytecode and simple WITH_EXCEPT_START bytecode support.

---
+++ tests/input/swap.py
@@ -0,0 +1,12 @@
+def SWAP():
+    my_array = [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        8]
+    i = 1
+    j = 3
+    my_array[i], my_array[j], my_array[2] = my_array[j], my_array[i], my_array[4]
